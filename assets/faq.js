2bf38265740b36a0e237a2ba148aa62a8e989bc0
ijungleboy <daniel.mettler@2sic.com>
2019-12-05 16:05:11 +0100

    add all

---
+++ assets/faq.js
@@ -0,0 +1,40 @@
+﻿function initFaqSection(containerSelector, showEffect) {
+	var container = $("." + containerSelector);
+	$(".faq-question", container).click(function (e) {
+	    var answer;
+		switch(showEffect) {
+			case "slide":
+				answer = $(e.target).closest(".faq-set").find(".faq-answer");
+				answer.slideToggle();
+				e.preventDefault();
+				break;
+			case "lightbox":
+			    var question = $(e.target);
+			    answer = question.next();
+			    answer.dialog({
+			        title: question.text(),
+			        autoOpen: true,
+			        dialogClass: "dnnFormPopup",
+                    modal: true
+			    });
+			default:
+				break;
+		}
+	});
+
+	// Attach drop-down filter
+	$("#ddlFeatureFilter", container).change(function (changeEvent) {
+		var tagFilter = changeEvent.target[changeEvent.target.selectedIndex].value;
+		console.log("tf:" + tagFilter);
+		//alert(tagFilter);
+		$(".faq-set", container).each(function (i, e) {
+			var tags = ($(e).attr('data-tags') + ",all").split(',');
+			console.log(tags);
+			if ($.inArray(tagFilter, tags) != -1)// || tagFilter == "all")
+				$(e).slideDown();
+			else
+				$(e).slideUp();
+		});
+	});
+
+}
